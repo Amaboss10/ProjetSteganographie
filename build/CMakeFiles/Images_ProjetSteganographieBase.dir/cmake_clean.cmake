746232abc56f56604fe0e7d39b2c29abcbdc5562
Amadou Diarra <amadiarra01@gmail.com>
2025-05-09 00:52:05 +0200

    update  gitignore

--- build/CMakeFiles/Images_ProjetSteganographieBase.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Images_ProjetSteganographieBase"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Images_ProjetSteganographieBase.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/Images_ProjetSteganographieBase.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Images_ProjetSteganographieBase.
-# This may be replaced when dependencies are built.
